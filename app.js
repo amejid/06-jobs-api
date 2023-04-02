ebd0b698b73225765faa0f3bf8e81d6ab68556bd
amejid <abdelmejid89@gmail.com>
2023-04-02 13:12:16 +0300

    Answers for lesson 12

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.use(express.static('public'));
+
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
